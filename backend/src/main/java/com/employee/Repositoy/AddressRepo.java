c0cf607f4f8ebad71fc330f9733a80757b162f8a
Prasad Patil <prasadpatila99@gmail.com>
2025-10-24 18:58:18 +0530

    backend updateed

--- backend/src/main/java/com/employee/Repositoy/AddressRepo.java
+++
@@ -1,9 +0,0 @@
-package com.employee.Repositoy;
-
-import com.employee.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepo extends JpaRepository<Address, Integer> {
-}
